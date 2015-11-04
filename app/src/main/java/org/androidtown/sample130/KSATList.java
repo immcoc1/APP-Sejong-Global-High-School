ceb8433586a7fc7bc20aa92f0b904c55b9c68fc9
immcoc1 <immcoc1@naver.com>
2015-11-04 21:10:55 +0900

    11.04 Edit

--- app/src/main/java/org/androidtown/sample130/KSATList.java
+++ app/src/main/java/org/androidtown/sample130/KSATList.java
@@ -1,5 +1,7 @@
 package org.androidtown.sample130;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +15,36 @@ public class KSATList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ksatlist);
+
+    }
+
+    public void onBackPressed() {
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("앱을 종료하시겠습니까?");
+        builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        android.app.AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void OnClickBackBtn(View v) {
+
+        Intent intent = new Intent(getApplicationContext(),Startmenu.class);
+        startActivity(intent);
+        finish();
 
     }
 
